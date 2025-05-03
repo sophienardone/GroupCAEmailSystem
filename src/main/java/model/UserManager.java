6d48c11e4af704ebdf6dd345b904cda5266f4e11
jamzy75 <jamzymup@gmail.com>
2025-05-03 04:30:18 +0100

    usermanger class to manage users.

---
+++ src/main/java/model/UserManager.java
@@ -0,0 +1,35 @@
+package model;
+
+import utils.SecurityUtils;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManager {
+    private Map<String, User> users;
+
+    public UserManager() {
+        this.users = new HashMap<>();
+    }
+
+    public boolean registerUser(String username, String rawPassword) {
+        if (users.containsKey(username)) return false;
+        String hashedPassword = SecurityUtils.hashPassword(rawPassword);
+        users.put(username, new User(username, hashedPassword));
+        return true;
+    }
+
+    public boolean authenticate(String username, String rawPassword) {
+        User user = users.get(username);
+        if (user == null) return false;
+        String hashedPassword = SecurityUtils.hashPassword(rawPassword);
+        return user.getPassword().equals(hashedPassword);
+    }
+
+    public boolean userExists(String username) {
+        return users.containsKey(username);
+    }
+
+    public User getUser(String username) {
+        return users.get(username);
+    }
+}

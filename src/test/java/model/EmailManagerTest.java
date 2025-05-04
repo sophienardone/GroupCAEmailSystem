1aced4ad20c18a857b07509decbc148043c89252
JulieOlamijuwon <d00215779@student.dkit.ie>
2025-05-04 22:03:58 +0100

    Commit: More Testing

--- src/test/java/model/EmailManagerTest.java
+++ src/test/java/model/EmailManagerTest.java
@@ -18,11 +18,12 @@ class EmailManagerTest {
     }
     @Test
     void addToSentEmailTest() {
+        System.out.println("Testint Email added successfully to sent emails");
         boolean result = emailManager.addToSentEmail(testEmail);
         String sender = "Joan";
-        assertTrue(result, "Email should be added successfully to sent emails");
-
-        List<Email> sent = emailManager.getSentEmailsForUser("Joan");
+        assertTrue(result);
+        //Email should be added successfully to sent emails
+        List<Email> sent = emailManager.getSentEmailsForUser(sender);
         assertNotNull(sent);
         assertEquals(1, sent.size());
         assertEquals(testEmail, sent.get(0));
@@ -30,7 +31,28 @@ class EmailManagerTest {
     }
 
     @Test
-    void addToReceivedEmailVTest() {
+    void addToReceivedEmailTest() {
+        System.out.println(" Testing Email added successfully to Recieved emails");
+        boolean result = emailManager.addToReceivedEmail(testEmail);
+        String reciever = "Peter";
+        assertTrue(result);
+
+        List<Email> recieved = emailManager.getReceivedEmailsForUser(reciever);
+        assertNotNull(recieved);
+        assertEquals(1, recieved.size());
+        assertEquals(testEmail, recieved.get(0));
+    }
+    @Test
+    void addToReceivedEmailFailedTest() {
+        System.out.println(" Testing Adding Email to Recieved emails FAILED");
+        Email Null = null;
+
+        boolean result = emailManager.addToReceivedEmail(Null);
+        assertFalse(result); // "Adding a null email should return false"
+
+        List<Email> received = emailManager.getReceivedEmailsForUser("Peter");
+        assertTrue(received == null || received.isEmpty()); // "No emails should be added for a null email"
+
     }
 
     @Test
